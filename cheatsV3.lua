c5c71f529d66f2d7700f56a1d0c0932c562fdcb9
Eri-Yoshimi <yuuki.sakura.ys@gmail.com>
2025-04-04 21:57:27 +0200

    420

--- cheatsV3.lua
+++ cheatsV3.lua
@@ -73,7 +73,7 @@ end
 
 local Style = {
 	name = "No Big Deal script by Eri",
-	size = UDim2.new(0, 600, 0, 400),
+	size = UDim2.new(0, 600, 0, 420),
 	primaryColor = Color3.new(0.615686, 0.47451, 0.305882),
 	secondaryColor = Color3.new(0.670588, 0.584314, 0.388235),
 	backgroundColor = Color3.new(0, 0, 0),
